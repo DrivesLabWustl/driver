6db7cf4282f1bcc58525c321ff0e2b58bdab4c32
Matthew Schuelke <matt@themadstatter.com>
2021-10-13 13:13:28 -0500

    p2 per zcta

--- R/census.R
+++ R/census.R
@@ -9,7 +9,20 @@
 #'
 #' @examples
 #' \dontrun{
-#' roe_get_census_urban_rural_per_block_group_2010("01")
+#' ## data for all block groups in Missouri
+#' roe_get_census_urban_rural_per_block_group_2010("29")
+#'
+#' ## data for all block groups
+#' fips_codes_for_states_and_dc %>%
+#'   mutate(data = purrr::map(state_fips_code, roe_get_census_urban_rural_per_block_group)) %>%
+#'   tidyr::unnest(cols = c(data))
+#'
+#' ## compute percents urban and rural for each Missouri block group
+#' roe_get_census_urban_rural_per_block_group("29") %>%
+#'   mutate(
+#'     cen_bgrp_pct_urban_2010 = 100 * cen_bgrp_pop_urban_2010 / cen_bgrp_pop_total_2010,
+#'     cen_bgrp_pct_rural_2010 = 100 * cen_bgrp_pop_rural_2010 / cen_bgrp_pop_total_2010
+#'   )
 #' }
 #'
 #' @details
@@ -53,5 +66,65 @@ roe_get_census_urban_rural_per_block_group <- function(
       !!paste0("cen_bgrp_pop_uarea_", vintage) := P002003,
       !!paste0("cen_bgrp_pop_uclst_", vintage) := P002004,
       !!paste0("cen_bgrp_pop_rural_", vintage) := P002005
+    )
+}
+
+#' Retrieve Urban and Rural (p2) Summary File 1 (sf1) Census data
+#'
+#' @param vintage = Year of dataset, e.g., 2000. This data is only collected during decennial censuses.
+#' @param key = Your Census API key, requested using \url{https://api.census.gov/data/key_signup.html}.
+#'
+#' @return A tibble with P2 variables by zip code tabulation area.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ## data for all zip code tabulation areas
+#' roe_get_census_urban_rural_per_zip_code_tabulation_area()
+#'
+#' ## compute percents urban and rural for each zip code tabulation area
+#' roe_get_census_urban_rural_per_zip_code_tabulation_area() %>%
+#'   mutate(
+#'     cen_zcta_pct_urban_2010 = 100 * cen_zcta_pop_urban_2010 / cen_zcta_pop_total_2010,
+#'     cen_zcta_pct_rural_2010 = 100 * cen_zcta_pop_rural_2010 / cen_zcta_pop_total_2010
+#'   )
+#' }
+#'
+#' @details
+#' Decennial Census API Documentation
+#' \url{https://www.census.gov/data/developers/data-sets/decennial-census.html}
+#'
+#' URBAN AND RURAL variables listed in:
+#' \url{https://api.census.gov/data/2010/dec/sf1/variables.html}
+#' \tabular{llllllll}{
+#' \strong{Name} \tab \strong{Label} \tab \strong{Concept} \tab \strong{Required} \tab \strong{Attributes} \tab \strong{Limit} \tab \strong{Predicate Type} \tab \strong{Group} \cr
+#' P002001 \tab Total	                                \tab URBAN AND RURAL \tab not required \tab P002001ERR \tab 0 \tab int  \tab P2 \cr
+#' P002002 \tab Total!!Urban	                        \tab URBAN AND RURAL \tab not required \tab            \tab 0 \tab int  \tab P2 \cr
+#' P002003 \tab Total!!Urban!!Inside urbanized areas  \tab URBAN AND RURAL \tab not required \tab            \tab 0 \tab int  \tab P2 \cr
+#' P002004 \tab Total!!Urban!!Inside urban clusters	  \tab URBAN AND RURAL \tab not required \tab            \tab 0 \tab int  \tab P2 \cr
+#' P002005 \tab Total!!Rural	                        \tab URBAN AND RURAL \tab not required \tab            \tab 0 \tab int  \tab P2
+#' }
+roe_get_census_urban_rural_per_zip_code_tabulation_area <- function(
+  vintage = c("2010", "2000"),
+  key = Sys.getenv("CENSUS_KEY")
+) {
+  vintage <- match.arg(vintage)
+
+  censusapi::getCensus(
+    name = "dec/sf1",
+    vintage = vintage,
+    key = key,
+    vars = c("GEO_ID", "NAME", "P002001", "P002002", "P002003", "P002004", "P002005"),
+    region = "zip code tabulation area:*"
+  ) %>%
+    dplyr::rename(
+      !!paste0("cen_zcta_zcta_",      vintage) := zip_code_tabulation_area,
+      !!paste0("cen_zcta_geoid_",     vintage) := GEO_ID,
+      !!paste0("cen_zcta_name_",      vintage) := NAME,
+      !!paste0("cen_zcta_pop_total_", vintage) := P002001,
+      !!paste0("cen_zcta_pop_urban_", vintage) := P002002,
+      !!paste0("cen_zcta_pop_uarea_", vintage) := P002003,
+      !!paste0("cen_zcta_pop_uclst_", vintage) := P002004,
+      !!paste0("cen_zcta_pop_rural_", vintage) := P002005
     )
 }
