a531c096e1f58f053da8998047bffd73972d7e75
Matthew Schuelke <matt@themadstatter.com>
2022-03-31 12:14:08 -0500

    tunnel arg fix

--- R/utilities.R
+++ R/utilities.R
@@ -45,6 +45,6 @@ tunnel_to_database_server <-
 
     system2(
       "xterm",
-      sprintf("-e ssh -4 -L 3306:127.0.0.1:3306 %s@%s", user, db)
+      sprintf("-e ssh -4 -L 3306:127.0.0.1:3306 %s@%s", user, server)
     )
   }
