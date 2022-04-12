826aacd929e2c60a5d9cf2dbacd9f29cedfcbc56
Matthew Schuelke <matt@themadstatter.com>
2022-04-12 11:48:54 -0500

    new kegg fns

--- R/adrc.R
+++ R/adrc.R
@@ -1,19 +1,71 @@
-#' Lookup Drug Codes by Name from Kegg.jp
+#' Lookup Drug Information from Kegg.jp
 #'
-#' @param drug_name The name of the drug to lookup.
+#' @param x either the drug name or code
 #'
 #' @return A tibble listing drug codes and corresponding names.
 #' @export
 #'
 #' @examples
-#' get_drug_codes("acetaminophen")
+#' kegg_drug_info("acetaminophen")
+#'
 #' @references
 #' <https://www.kegg.jp/kegg/drug/>
-get_drug_codes <- function(drug_name) {
-  sprintf("http://rest.kegg.jp/find/drug/%s", drug_name) %>%
+kegg_drug_info <- function(x) {
+  sprintf("http://rest.kegg.jp/find/drug/%s", x) %>%
     httr::GET() %>%
     httr::content() %>%
     readr::read_delim(delim = "\t", col_names = FALSE, col_types = "cc") %>%
-    dplyr::rename(drug_code = .data$X1, drug_name = .data$X2) %>%
-    dplyr::mutate(drug_code = sub("^dr:D", "d", .data$drug_code))
+    dplyr::rename(code = .data$X1, name = .data$X2) %>%
+    dplyr::mutate(code = sub("^dr:D", "d", .data$code))
+}
+
+#' Lookup Drug Name from Code via Kegg.jp
+#'
+#' @param code drug code
+#'
+#' @return character descriptor of drug code
+#' @export
+#'
+#' @examples
+#' kegg_drug_name("d00217")
+#' @references
+#' <https://www.kegg.jp/kegg/drug/>
+kegg_drug_name <- function(code) {
+  kegg_drug_info(code)$name
+}
+
+#' Lookup Drug Code(s) from Name via Kegg.jp
+#'
+#' @param name drug name
+#' @param formal formal Kegg name
+#'
+#' @return character vector of drug code(s)
+#' @export
+#'
+#' @examples
+#' ## lookup all associated codes
+#' kegg_drug_code('acetaminophen')
+#'
+#' ## lookup code for formal name
+#' kegg_drug_code(
+#'   'acetaminophen',
+#'   'Acetaminophen (JP18/USP); Paracetamol (INN); Tylenol (TN)'
+#'  )
+#'
+#' @references
+#' <https://www.kegg.jp/kegg/drug/>
+#'
+#' @note Searching via name is often one-to-many. Use `kegg_drug_info()` to get
+#' names associated with multiple codes. Results will be filtered to match
+#' formal name if supplied.
+kegg_drug_code <- function(name, formal = "") {
+  results <- kegg_drug_info(name)
+
+  if(nchar(formal) > 0) {
+    results %>%
+      dplyr::filter(name == formal) %>%
+      dplyr::pull(code)
+  } else {
+    results$code
+  }
 }
